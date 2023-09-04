e2fac9ffc559392cb628b296e8e15e81b310d230
Thorzuck01 <143899328+Thorzuck01@users.noreply.github.com>
2023-09-04 16:13:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+var text = document.getElementById("ftext");
+var listContainer = document.getElementById("list-container");
+
+
+function addtask(){
+    
+    if(text.value == ""){
+        window.alert("Você não digitou nada")
+
+    }else{
+       var listItem = document.createElement("li");
+       listItem.innerHTML = text.value;
+       listContainer.appendChild(listItem);
+       var span = document.createElement("span");
+       span.innerHTML = "\u00d7";
+       listItem.appendChild(span)
+      
+    }
+    text.value = " ";
+
+    
+
+}
+
+listContainer.addEventListener("click",function(e){
+   
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+    saveData();
+
+
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showtask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showtask();
+
